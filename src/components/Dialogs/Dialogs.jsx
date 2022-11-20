b0814cb28dc85ff6567b1220ecfd65bebc7a1850
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-19 21:00:23 -0500

    start work on messages component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,17 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) =>{
     return(
-        <div>
-            Dialogs
+        <div className='dialogs'>
+            <div className='dialogs-item'>
+                <div className='dialog'>Leo</div>
+                <div className='dialog'>Maria</div>
+                <div className='dialog'>Stan</div>
+            </div>
+            <div className='messages'>
+                <div className='message'>Hi, how are you</div>
+                <div className='message'>Come with me to cinema</div>
+                <div className='message'>I am going to shopping do you wanna go?</div>
+            </div>
         </div>
     )
 }
