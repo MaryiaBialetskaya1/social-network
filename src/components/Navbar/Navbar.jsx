a8c4e76946901798cd1c794186cbf6abb4aabc45
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-11-19 20:47:45 -0500

    add className active and styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,10 +6,10 @@ const Navbar = () =>{
     return(
         <nav className={s.nav}>
             <div className={`${s.item} ${s.active}`}>
-                <NavLink to='/profile'>Profile</NavLink>
+                <NavLink to='/profile' className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to='/dialogs'>Messages</NavLink>
+                <NavLink to='/dialogs' className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
             </div>
             <div className={s.item}>
                 <NavLink to='src/components/Navbar/Navbar#debugging'>News</NavLink>
