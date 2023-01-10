52dd45d7d4180c5cc6d18c3b5b40b685a60f8dac
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-10 13:02:18 -0500

    divide Profile.jsx to components

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 // import s from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
         <div>
-            <div>
-                <img
-                    src='https://images.unsplash.com/photo-1489535952530-0fd16a62e111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGplbGx5ZmlzaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
-                    alt='main page'/>
-            </div>
-            <div>
-                <img src='src/components/Profile/Profile' alt='avatar'/>
-                <div>Description</div>
-            </div>
+            <ProfileInfo />
             <MyPosts/>
         </div>
     )
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import s from './ProfileInfo.module.css';
+
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img
+                    src='https://images.unsplash.com/photo-1489535952530-0fd16a62e111?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGplbGx5ZmlzaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+                    alt='main page'/>
+            </div>
+            <div>
+                <img src='src/components/Profile/Profile' alt='avatar'/>
+                <div>Description</div>
+            </div>
+        </div>
+    )
+}
+export default ProfileInfo;
