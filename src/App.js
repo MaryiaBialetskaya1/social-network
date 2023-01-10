fdfef83620f95dc45bdf68d10205568abe1b15d1
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-10 11:55:13 -0500

    add asterisk to App.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () => {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Routes>
-                        <Route path="/profile" element={<Profile/>}/>
-                        <Route path="/dialogs" element={<Dialogs/>}/>
+                        <Route path="/profile/*" element={<Profile/>}/>
+                        <Route path="/dialogs/*" element={<Dialogs/>}/>
                     </Routes>
                     {/*{<Dialogs/>}*/}
                     {/*{<Profile/>}*/}
