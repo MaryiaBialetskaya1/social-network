d002e0f6307240afa610d55c177efc490680c911
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-11 15:43:23 -0500

    add styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,20 +5,22 @@ import {NavLink} from "react-router-dom";
 const Navbar = () =>{
     return(
         <nav className={s.nav}>
-            <div className={`${s.item} ${s.active}`}>
-                <NavLink to='/profile' className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='/dialogs' className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='src/components/Navbar/Navbar#debugging'>News</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='src/components/Navbar/Navbar#debugging'>Music</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to='src/components/Navbar/Navbar#debugging'>Settings</NavLink>
+            <div className={s.block}>
+                <div className={`${s.item} ${s.active}`}>
+                    <NavLink to='/profile' className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to='/dialogs' className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to='src/components/Navbar/Navbar#debugging'>News</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to='src/components/Navbar/Navbar#debugging'>Music</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to='src/components/Navbar/Navbar#debugging'>Settings</NavLink>
+                </div>
             </div>
         </nav>
     )
