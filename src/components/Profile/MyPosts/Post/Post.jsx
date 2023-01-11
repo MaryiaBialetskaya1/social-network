3923262daba7bce265b7b62f64aed0ac38b181f2
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-11 15:10:26 -0500

    add styles

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -3,14 +3,20 @@ import s from './Post.module.css';
 
 const Post = (props) => {
     return (
-        <div className={s.item}>
-            <img
-                //src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
-                src='https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGFtc3RlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
-                alt='portrait'/>
-            {props.message}
+        <div className={s.myPostBlock}>
             <div>
-                <span>like</span> {props.likesCount}
+                <img
+                    //src='https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
+                    src='https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGFtc3RlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+                    alt='portrait'/>
+            </div>
+            <div className={s.postItemBlock}>
+                <div>
+                    {props.message}
+                </div>
+                <div className={s.likeSign}>
+                    <span>&#x2661;</span> {props.likesCount}
+                </div>
             </div>
         </div>
     )
--- src/redux/state.js
+++ src/redux/state.js
@@ -25,11 +25,11 @@
 let state = {
     profilePage: {
         posts: [
-            {id: 1, message: "Hi, how are you", likesCount: 12},
-            {id: 2, message: "Come with me to cinema", likesCount: 11},
-            {id: 3, message: "Hi, how are you", likesCount: 1},
-            {id: 4, message: "Hi, how are you", likesCount: 2},
-            {id: 5, message: "Hi, how are you", likesCount: 12},
+            {id: 1, message: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs", likesCount: 12},
+            {id: 2, message: "If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt", likesCount: 11},
+            {id: 3, message: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey", likesCount: 1},
+            {id: 4, message: "Life is what happens when you're busy making other plans. -John Lennon", likesCount: 2},
+            {id: 5, message: "Always remember that you are absolutely unique. Just like everyone else. -Margaret Mead\n", likesCount: 12},
         ],
     },
     dialogsPage: {
