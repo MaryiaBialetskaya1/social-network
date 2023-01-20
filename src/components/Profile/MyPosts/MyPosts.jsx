9693c4d938eb43469d450297e0e6e123411b7aee
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-20 10:43:27 -0500

    create new functions ActionCreator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
+let addPostActionCreator = () => {
+    return {
+        type: 'ADD-POST',
+    }
+}
+let updateNewPostTextActionCreator = (text) => {
+    return {
+        type: 'UPDATE-NEW-POST-TEXT',
+        newText: text,
+    }
+}
 const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPostButton = () => {
-        props.dispatch({type: 'ADD-POST'});
+        props.dispatch(addPostActionCreator());
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text};
+        let action = updateNewPostTextActionCreator(text);
         props.dispatch(action);
     }
 
