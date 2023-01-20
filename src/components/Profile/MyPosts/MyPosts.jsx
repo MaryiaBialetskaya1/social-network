53456ae5fb0f095467782e41a12d8febb1288f40
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-20 10:40:02 -0500

    delete debugger;

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,6 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-debugger;
     let newPostElement = React.createRef();
 
     let addPostButton = () => {
