b2c093cf79078d2a03781ebd3e2a1947c859cfdf
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-20 10:47:42 -0500

    add imports of ActionCreator functions to MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
 
 
 const MyPosts = (props) => {
