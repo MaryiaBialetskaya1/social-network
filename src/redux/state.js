7b47866298c5a1d864852e26c69ac97a0ea4a5aa
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-21 19:32:42 -0500

    add new else if condition with action send message

--- src/redux/state.js
+++ src/redux/state.js
@@ -58,6 +58,11 @@ let store = {
         } else if(action.type === UPDATE_NEW_MESSAGE_BODY){
             this._state.dialogsPage.newMessageBody = action.body;
             this._callSubscriber(this._state);
+        } else if(action.type === SEND_MESSAGE){
+            let body = this._state.dialogsPage.newMessageBody;
+            this._state.dialogsPage.newMessageBody = '';
+            this._state.dialogsPage.messages.push({id: 4, message: body})
+            this._callSubscriber(this._state);
         }
     }
 }
