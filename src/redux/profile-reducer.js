b691d6f6cf5f0d19925961849676816f241a0d5b
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-22 14:43:36 -0500

    replace else if with switch

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -2,18 +2,22 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const profileReducer = (state, action) => {
-debugger;
-    if(action.type === ADD_POST){
-        let newPost = {
-            id: 1,
-            message: state.newPostText,
-            likesCount: 0
-        }
-        state.newPostText = "";
-        state.posts.push(newPost);
-    } else if(action.type === UPDATE_NEW_POST_TEXT){
-        state.newPostText = action.newText;
+
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 1,
+                message: state.newPostText,
+                likesCount: 0
+            }
+            state.newPostText = "";
+            state.posts.push(newPost);
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newText;
+            return state;
+        default:
+            return state;
     }
-    return state;
 }
 export default profileReducer;
