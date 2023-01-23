8ff4dfc2800ff271ad5aeb4b7f828a43b9f2a6ee
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-23 13:31:30 -0500

    rename file state.js to store.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from "./redux/state";
+import store from "./redux/store";
 
 
 
